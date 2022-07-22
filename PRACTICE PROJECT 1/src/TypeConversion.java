197fbaa4e779ecc43fa0b3b67fca6d9fa0ec01bc
K S MURALI <85218666+MURALI4121@users.noreply.github.com>
2022-07-23 00:06:10 +0530

    Update TypeConversion.java

--- PRACTICE PROJECT 1/src/TypeConversion.java
+++ PRACTICE PROJECT 1/src/TypeConversion.java
@@ -1,19 +1,36 @@
 
-public class TypeConversion {
-	  public static void main(String[] args) 
-	  { 
-	    byte b = 42; 
-	    char c = 'a'; 
-	    short s = 1024; 
-	    int i = 50000; 
-	    float f = 5.67f; 
-	    double d = .2; 
+public class typeCasting {
 
-	// Expression: 
-	   double result = (f * b) + (i / c) - (d * s); 
-
-	//Result after all the conversions are done. 
-	   System.out.println("result = " + result); 
-	  } 
+	public static void main(String[] args) {
+		
+		//implicit conversion
+		System.out.println("Implicit Type Casting");
+		char a='A';
+		System.out.println("Value of a: "+a);
+		
+		int b=a;
+		System.out.println("Value of b: "+b);
+		
+		float c=a;
+		System.out.println("Value of c: "+c);
+		
+		long d=a;
+		System.out.println("Value of d: "+d);
+		
+		double e=a;
+		System.out.println("Value of e: "+e);
+		
+				
+		System.out.println("\n");
+		
+		System.out.println("Explicit Type Casting");
+		//explicit conversion
+		
+		double x=45.5;
+		int y=(int)x;
+		System.out.println("Value of x: "+x);
+		System.out.println("Value of y: "+y);
+		
+	}
 
 }
