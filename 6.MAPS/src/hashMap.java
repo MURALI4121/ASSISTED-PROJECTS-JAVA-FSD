2354081f94223aaeec1086ec35c72e54ed7b30f9
MURALI4121 <muralishankar556@gmail.com>
2022-07-23 15:00:33 +0530

    first commit

---
+++ 6.MAPS/src/hashMap.java
@@ -0,0 +1,24 @@
+
+
+	import java.util.*;
+	public class hashMap {
+
+		public static void main(String[] args) {
+			// map
+			
+			//Hashmap
+			HashMap<Integer,String> hm=new HashMap<Integer,String>();      
+		      hm.put(1,"Tim");    
+		      hm.put(2,"Mary");    
+		      hm.put(3,"Catie");   
+		       
+		      System.out.println("\nThe elements of Hashmap are ");  
+		      for(Map.Entry m:hm.entrySet()){    
+		       System.out.println(m.getKey()+" "+m.getValue());    
+		      }
+		      
+		     
+		      }
+		}
+	
+
---
+++ 6.MAPS/src/hashTableDemo.java
@@ -0,0 +1,22 @@
+import java.util.Hashtable;
+import java.util.Map;
+
+public class hashTableDemo {
+
+	public static void main(String[] args) {
+      Hashtable<Integer,String> ht=new Hashtable<Integer,String>();  
+	      
+	      ht.put(4,"Ales");  
+	      ht.put(5,"Rosy");  
+	      ht.put(6,"Jack");  
+	      ht.put(7,"John");  
+
+	      System.out.println("\nThe elements of HashTable are ");  
+	      for(Map.Entry n:ht.entrySet()){    
+	       System.out.println(n.getKey()+" "+n.getValue());    
+	      }
+
+
+	}
+
+}
