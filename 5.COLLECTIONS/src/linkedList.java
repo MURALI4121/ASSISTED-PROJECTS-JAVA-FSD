29d2b5c67f581ab8571ef9b1a08127ca04e4a569
MURALI4121 <muralishankar556@gmail.com>
2022-07-23 14:59:49 +0530

    first commit

---
+++ 5.COLLECTIONS/src/linkedList.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class linkedList {
+
+	public static void main(String[] args) {
+		System.out.println("\n");
+	      System.out.println("LinkedList");
+	      LinkedList<String> names=new LinkedList<String>();  
+	      names.add("Alex");  
+	      names.add("John");  	      
+	      Iterator<String> itr=names.iterator();  
+	      while(itr.hasNext()){  
+	       System.out.println(itr.next());  
+	       
+
+	}
+  }
+}
---
+++ 5.COLLECTIONS/src/vector.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class vector {
+
+		public static void main(String[] args) {
+			//creating arraylist
+		      System.out.println("\n");
+		      System.out.println("Vector");
+		      Vector<Integer> vec = new Vector();
+		      vec.addElement(15); 
+		      vec.addElement(30); 
+		      System.out.println(vec);
+
+	}
+
+}
